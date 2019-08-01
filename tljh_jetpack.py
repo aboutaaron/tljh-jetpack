cc5d92d93e36da0ca17c13939a59b851c9ff4004
Aaron Williams <aaron.colby.williams@gmail.com>
2019-08-01 19:02:01 -0400

    Add logging

--- tljh_jetpack.py
+++ tljh_jetpack.py
@@ -16,7 +16,41 @@ from tljh.config import (
   USER_ENV_PREFIX
 )
 
+
+"""
+Adding a custom logging class so I know when jetpack is running
+See: https://stackoverflow.com/a/56944256/868724
+"""
+class CustomFormatter(logging.Formatter):
+    """Logging Formatter to add colors and count warning / errors"""
+
+    grey = "\x1b[38;21m"
+    blue = '\033[94m'
+    yellow = "\x1b[33;21m"
+    red = "\x1b[31;21m"
+    bold_red = "\x1b[31;1m"
+    reset = "\x1b[0m"
+    format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s (%(filename)s:%(lineno)d)"
+
+    FORMATS = {
+        logging.DEBUG: grey + format + reset,
+        logging.INFO: blue + format + reset,
+        logging.WARNING: yellow + format + reset,
+        logging.ERROR: red + format + reset,
+        logging.CRITICAL: bold_red + format + reset
+    }
+
+    def format(self, record):
+        log_fmt = self.FORMATS.get(record.levelno)
+        formatter = logging.Formatter(log_fmt)
+        return formatter.format(record)
+
 logger = logging.getLogger("tljh")
+logger.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setFormatter(CustomFormatter())
+logger.addHandler(ch)
+
 
 @hookimpl
 def tljh_extra_user_conda_packages():
